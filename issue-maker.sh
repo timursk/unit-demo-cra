97d3db19727d930e24c8c5054c99ac31a19dc4cd
Timur Salikhov <timur.dev@mail.ru>
2023-07-16 19:55:50 +0300

    fix: issue_template error

--- issue-maker.sh
+++ issue-maker.sh
@@ -4,7 +4,7 @@ echo $GITHUB_ACTOR
 NOW=`node -e "console.log(new Date())"`
 DASH_LINE="---"
 echo "$DASH_LINE" >ISSUE.md
-echo "title: Release tag: $REF_NAME" >>ISSUE.md
+echo "title: Release tag $REF_NAME" >>ISSUE.md
 echo "labels: RELEASE" >>ISSUE.md
 echo "$DASH_LINE" >>ISSUE.md
 echo "Release $REF_NAME by $ACTOR ($NOW)." >>ISSUE.md
