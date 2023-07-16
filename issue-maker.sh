120754e2a5f1398c1fe2c48f5567eee38c5e684a
Timur Salikhov <timur.dev@mail.ru>
2023-07-16 19:38:17 +0300

    fix: extension case

--- issue-maker.sh
+++ issue-maker.sh
@@ -3,4 +3,4 @@
 echo $GITHUB_ACTOR
 NOW=`node -e "console.log(new Date())"`
 DASH_LINE="---"
-echo -e "$DASH_LINE\ntitle:Release tag: $REF_NAME\nlabels: RELEASE\n$DASH_LINE\nRelease $REF_NAME by $ACTOR ($NOW)." > ISSUE.MD
+echo -e "$DASH_LINE\ntitle:Release tag: $REF_NAME\nlabels: RELEASE\n$DASH_LINE\nRelease $REF_NAME by $ACTOR ($NOW)." > ISSUE.md
