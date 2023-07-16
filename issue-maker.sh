4816e5c43160c252950df051fedef0dca86bcadd
Timur Salikhov <timur.dev@mail.ru>
2023-07-16 19:50:49 +0300

    refactor: rewrite echo multiline

--- issue-maker.sh
+++ issue-maker.sh
@@ -3,4 +3,8 @@
 echo $GITHUB_ACTOR
 NOW=`node -e "console.log(new Date())"`
 DASH_LINE="---"
-echo -e "$DASH_LINE\ntitle:Release tag: $REF_NAME\nlabels: RELEASE\n$DASH_LINE\nRelease $REF_NAME by $ACTOR ($NOW)." > ISSUE.md
+echo "$DASH_LINE" >ISSUE.md
+echo "title:Release tag: $REF_NAME" >>ISSUE.md
+echo "labels: RELEASE" >>ISSUE.md
+echo "$DASH_LINE" >>ISSUE.md
+echo "Release $REF_NAME by $ACTOR ($NOW)." >>ISSUE.md
